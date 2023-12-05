29cd2dab5f318096f15c1d0f13fd03bbe54ecdfd
vaish28 <madhekarvaishnavi@gmail.com>
2023-12-04 19:03:07 -0500

    submission

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const CONNECTION_STRING =
 	process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
 
 
-await mongoose.connect(CONNECTION_STRING,  { useNewUrlParser: true, useUnifiedTopology: true })
+await mongoose.connect(CONNECTION_STRING)
 
 app.use(
 	cors({
